50d2d185d383bd9c7b1c869b893d52d0cae22488
Benjamin Lee <benjaminlee0727@gmail.com>
2022-08-26 14:47:22 -0500

    Complete candy model and export

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -1,6 +1,27 @@
-const Sequelize = require('sequelize');
-const db = require('../database');
+const Sequelize = require("sequelize");
+const db = require("../database");
 
-module.exports = db.define('candy', {
-  // define your model here!
+module.exports = db.define("candy", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    validate: {
+      min: 0,
+      max: 10,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      "https://upload.wikimedia.org/wikipedia/commons/7/70/Chocolate_%28blue_background%29.jpg",
+  },
 });
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
+        options: { presets: ["@babel/preset-react"] },
       },
     ],
   },
