953ea981fad9510466fdd7bf64e3668f20ec30d6
Benjamin Lee <benjaminlee0727@gmail.com>
2022-08-26 15:24:19 -0500

    added dummy routes for action creators

--- app/actions.js
+++ app/actions.js
@@ -3,30 +3,56 @@ const ADD_CANDY = "ADD_CANDY";
 const DELETE_CANDY = "DELETE_CANDY";
 const UPDATE_CANDY = "UPDATE_CANDY";
 
-const setCandy = (candies) => {
+const _setCandy = (candies) => {
   return {
     type: SET_CANDIES,
     candies,
   };
 };
 
-const createCandy = (candy) => {
+const _createCandy = (candy) => {
   return {
     type: ADD_CANDY,
     candy,
   };
 };
 
-const deleteCandy = (candy) => {
+const _deleteCandy = (candy) => {
   return {
     type: DELETE_CANDY,
     candy,
   };
 };
 
-const updateCandy = (candy) => {
+const _updateCandy = (candy) => {
   return {
     type: UPDATE_CANDY,
     candy,
+  };
+};
+
+export const setCandy = () => {
+  return async function (dispatch) {
+    // const {data: candies} = await axios.get(candy route)
+    dispatch(_setCandy(candies));
+  };
+};
+
+export const createCandy = (candy) => {
+  return async function (dispatch) {
+    // const {data: candy} = await axios.post(candy route)
+    dispatch(_createCandy(candy));
+  };
+};
+export const deleteCandy = (candy) => {
+  return async function (dispatch) {
+    // const {data: candy} = await axios.delete(candy route)
+    dispatch(_deleteCandy(candy));
+  };
+};
+export const updateCandy = (candy) => {
+  return async function (dispatch) {
+    // const {data: candy} = await axios.put(candy route)
+    dispatch(_updateCandy(candy));
   };
 };
--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
-const router = require('express').Router()
-
+const router = require("express").Router();
+const Candy = require("../db/models/Candy");
 // Your routes go here!
 // NOTE: Any routes that you put here are ALREADY mounted on `/api`
 // You can put all routes in this file HOWEVER,
@@ -18,10 +18,18 @@ const router = require('express').Router()
 // but you DON'T have a corresponding router, this piece of
 // middleware will generate a 404, and send it to your
 // error-handling endware!
-router.use((req, res, next) => {
-  const err = new Error('API route not found!')
-  err.status = 404
-  next(err)
-})
+router.get("/", async (req, res, next) => {
+  try {
+    res.send(":)");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-module.exports = router
+router.use((req, res, next) => {
+  const err = new Error("API route not found!");
+  err.status = 404;
+  next(err);
+});
+
+module.exports = router;
